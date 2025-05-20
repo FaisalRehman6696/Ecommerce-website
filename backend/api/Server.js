167789e8e8b1d868035f921462c9bdf01956d44b
FaisalRehman7 <fiasalrehman7934@gmail.com>
2025-05-20 20:51:37 +0500

    upload

--- backend/api/Server.js
+++ backend/api/Server.js
@@ -23,7 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use("/api",router);
+app.use("/",router);
 
 // app.listen(PORT, () => {
 //   console.log(`Server is running on ${PORT}`);
