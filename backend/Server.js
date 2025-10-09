ef753d749d457d79eb9558f14ca16cad15a1f65b
FaisalRehman7 <faisalrerhman7934@gmail.com>
2025-10-09 23:22:14 +0500

    n

--- backend/Server.js
+++ backend/Server.js
@@ -12,7 +12,17 @@ const app = express();
 
 Database();
 
-app.use(cors({ origin: "https://ecommerce-website-teal-theta.vercel.app" }));
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type"
+  );
+
+  next();
+});
 app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
