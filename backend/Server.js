fbfc1a78b9e21426f3e17ad1c36b65cdc6523b5e
FaisalRehman7 <faisalrerhman7934@gmail.com>
2025-10-09 18:43:35 +0500

    ok

--- backend/Server.js
+++ backend/Server.js
@@ -14,11 +14,24 @@ Database();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://ecommerce-website-omega-eight.vercel.app"],
+    origin: [
+      "https://ecommerce-website-omega-eight.vercel.app",
+      "http://localhost:5173",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    
   })
 );
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://ecommerce-website-omega-eight.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
