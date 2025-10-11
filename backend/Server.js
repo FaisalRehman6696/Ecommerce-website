dd16d6948ae50b62976a8fd63bda72257d7d59c7
FaisalRehman7 <faisalrerhman7934@gmail.com>
2025-10-11 20:50:08 +0500

    uhg

--- backend/Server.js
+++ backend/Server.js
@@ -7,17 +7,10 @@ import Database from "./config/db.js";
 import { fileURLToPath } from "url";
 import path from "path";
 const PORT = process.env.PORT 
-// import serverless from "serverless-http";
+
 const app = express();
 
 Database();
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-
 app.use(
   cors({
     origin: ["https://ecommerce-website-teal-theta.vercel.app", "http://localhost:5173"],
@@ -27,10 +20,17 @@ app.use(
   })
 );
 
-app.options("*", cors()); // Handle preflight requests globally
+app.options("*", cors()); 
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+
 app.use(router);
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
-// export default serverless(app);
+
