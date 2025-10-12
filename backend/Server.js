c16e9d159217348e90b0d4b87eef7f836ec9c804
FaisalRehman7 <faisalrerhman7934@gmail.com>
2025-10-12 17:25:16 +0500

    bh

--- backend/Server.js
+++ backend/Server.js
@@ -9,19 +9,24 @@ import path from "path";
 const PORT = process.env.PORT;
 const app = express();
 Database();
-app.use(cors());
-// https://ecommerce-website-3-tg4v.onrender.com/get-active-product
+app.use(
+  cors({
+    origin: [
+      "https://ecommerce-website-3-tg4v.onrender.com/get-active-product",
+    ],
+  })
+);
+
 app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-app.use(router);
-
-router.post("/test", (req, res) => {
+app.post("/test", (req, res) => {
   res.json({ msg: "chlado ab" });
 });
+app.use(router);
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
