a95c95e714aa8176b9f0f465be04317abed2f831
FaisalRehman7 <faisalrerhman7934@gmail.com>
2025-10-13 22:33:48 +0500

    wed

--- backend/Server.js
+++ backend/Server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
+// isko mehfooz kro
 app.use(router);
 
 app.listen(PORT, () => {
--- backend/api/Server.js
+++ backend/api/Server.js
@@ -20,7 +20,7 @@ app.use(
 );
 
 app.use(express.json());
-
+// isko b
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
